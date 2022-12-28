9ab05eb9a01ad351f1fa6ed5614962043db313e7
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2022-12-28 16:42:58 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -24,3 +24,43 @@ hum.addEventListener("click", () => {
 closem.addEventListener("click", () => {
   menu.classList.remove("active-2")
 })
+
+// Light On Sections
+
+let main = document.querySelectorAll(".wrap .container");
+let slide = document.querySelector(".links");
+let slideA = document.querySelectorAll(".links .link");
+
+function removeActive() {
+  slideA.forEach((ele) => {
+    ele.classList.remove("active-3");
+    ele.classList.remove("active");
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 0) {
+    removeActive();
+    slideA[0].classList.add("active-3");
+  }
+  if (scrollY >= main[1].offsetTop - 42) {
+    removeActive();
+    slideA[1].classList.add("active-3");
+  }
+  if (scrollY >= main[2].offsetTop - 42) {
+    removeActive();
+    slideA[2].classList.add("active-3");
+  }
+  if (window.scrollY >= main[3].offsetTop - 42) {
+    removeActive();
+    slideA[3].classList.add("active-3");
+  }
+  if (scrollY >= main[4].offsetTop - 42) {
+    removeActive();
+    slideA[4].classList.add("active-3");
+  }
+  if (scrollY >= main[5].offsetTop - 42) {
+    removeActive();
+    slideA[5].classList.add("active-3");
+  }
+});
