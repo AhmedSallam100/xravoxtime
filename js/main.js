144a95a27ae56607323e59333f4c3a7e3b93c600
Ahmed Sallam <109031729+AhmedSallam100@users.noreply.github.com>
2023-01-01 14:01:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,12 +27,12 @@ closem.addEventListener("click", () => {
 
 // Light On Sections
 
-let main = document.querySelectorAll(".wrap .container");
-let slide = document.querySelector(".links");
-let slideA = document.querySelectorAll(".links .link");
+let main1 = document.querySelectorAll(".wrap .container");
+let links2 = document.querySelector(".links");
+let link = document.querySelectorAll(".links .link");
 
 function removeActive() {
-  slideA.forEach((ele) => {
+  link.forEach((ele) => {
     ele.classList.remove("active-3");
     ele.classList.remove("active");
   });
@@ -41,26 +41,89 @@ function removeActive() {
 window.addEventListener("scroll", () => {
   if (scrollY >= 0) {
     removeActive();
-    slideA[0].classList.add("active-3");
+    link[0].classList.add("active-3");
   }
-  if (scrollY >= main[1].offsetTop - 42) {
+  if (scrollY >= main1[1].offsetTop - 42) {
     removeActive();
-    slideA[1].classList.add("active-3");
+    link[1].classList.add("active-3");
   }
-  if (scrollY >= main[2].offsetTop - 42) {
+  if (scrollY >= main1[2].offsetTop - 42) {
     removeActive();
-    slideA[2].classList.add("active-3");
+    link[2].classList.add("active-3");
   }
-  if (window.scrollY >= main[3].offsetTop - 42) {
+  if (window.scrollY >= main1[3].offsetTop - 42) {
     removeActive();
-    slideA[3].classList.add("active-3");
+    link[3].classList.add("active-3");
   }
-  if (scrollY >= main[4].offsetTop - 42) {
+  if (scrollY >= main1[4].offsetTop - 42) {
     removeActive();
-    slideA[4].classList.add("active-3");
+    link[4].classList.add("active-3");
   }
-  if (scrollY >= main[5].offsetTop - 42) {
+  if (scrollY >= main1[5].offsetTop - 42) {
     removeActive();
-    slideA[5].classList.add("active-3");
+    link[5].classList.add("active-3");
   }
 });
+
+// Light On Sections
+
+let main = document.querySelectorAll(".wrap .container");
+let slide = document.querySelector(".point-slide");
+let slideA = document.querySelectorAll(".point-slide a");
+
+function removeActive2() {
+  slideA.forEach((ele2) => {
+    ele2.classList.remove("active-4");
+  });
+}
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 0) {
+    removeActive2();
+    slideA[0].classList.add("active-4");
+  }
+  if (scrollY >= main[1].offsetTop - 72) {
+    removeActive2();
+    slideA[1].classList.add("active-4");
+  }
+  if (scrollY >= main[2].offsetTop - 72) {
+    removeActive2();
+    slideA[2].classList.add("active-4");
+  }
+  if (window.scrollY >= main[3].offsetTop - 72) {
+    removeActive2();
+    slideA[3].classList.add("active-4");
+  }
+  if (scrollY >= main[4].offsetTop - 72) {
+    removeActive2();
+    slideA[4].classList.add("active-4");
+  }
+  if (scrollY >= main[5].offsetTop - 72) {
+    removeActive2();
+    slideA[5].classList.add("active-4");
+  }
+});
+
+// Show Scroll
+
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-up");
+  if (this.scrollY >= 650) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
+
+function scrollTopWhats() {
+  const scrollTop = document.getElementById("whats");
+  if (this.scrollY >= 650) scrollTop.classList.add("show-whats");
+  else scrollTop.classList.remove("show-whats");
+}
+window.addEventListener("scroll", scrollTopWhats);
+
+// Animation Whats
+
+let whats = document.querySelector(".whats");
+
+setInterval(() => {
+  whats.classList.toggle("wobble");
+}, 1500);
